96d68aaef987148c8f5404c58d374598d689bd64
Taylor Jackson <taylor.daniel.jackson@outlook.com>
2020-03-03 21:38:33 -0500

    Added warning script alerting networking admin that a host is unreachable/down on network.

---
+++ Networking/node_down_warning_email.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+fir i in$@
+do
+ping -c 1 $i &> /dev/null
+
+#Substitute your own warning message as well as your email below.
+
+if [ $? -ne 0 ]; then
+	echo "`date`:ping to the node failed, $i node is down!" | mail -s "$i node is down!" name@mail.com
+	
+fi
+done
